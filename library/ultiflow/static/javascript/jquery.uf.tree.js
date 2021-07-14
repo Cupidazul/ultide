4cecfbfb5854d22dc309dfba2861755fce9ef9b2
Cupidazul <cupidazul@gmail.com>
2021-07-14 09:56:15 +0100

    minor fix

--- library/ultiflow/static/javascript/jquery.uf.tree.js
+++ library/ultiflow/static/javascript/jquery.uf.tree.js
@@ -40,57 +40,67 @@ define(['_', 'app', 'ultiflow', 'ultiflow-lib-jstree'], function(app, ultiflow) 
 
             self.element.jstree(options)
                 .on('ready.jstree', function(evt) {
+                    console.log('ready.jstree');
                     var el = $(evt.target);
-                    //console.log('ready.jstree:', el); //, el.find('ul.jstree-container-ul').find('li.jstree-node'));
 
-                    setTimeout(function() {
-                        //console.log('ready.jstree1:', el, $(document).find('ul.jstree-container-ul li.jstree-node ul.jstree-children').children());
-                        el.jstree('open_all'); // expand/open all jstree's
-                        $("a.jstree-anchor").each(function(ElIdx, ElVal) {
-                            //console.dir(ElVal);
-                            //console.log('ready.jstree.elm:', { id: ElVal.id, idx: ElIdx });
-                            if (new RegExp('.*::custom_process_anchor', 'g').test(ElVal.id)) {
-                                _.debounce(function(oElVal) {
-                                    console.log('OpenProjDebounced:', { id: oElVal.id, idx: ElIdx });
-                                    oElVal.click();
-                                }, 1000, { trailing: true })(ElVal);
-                            }
-                            //OpenProjDebounced();
-                            //} // open project
-                        });
-                        $('.main-view').css('left', '0px');
-                        window.$flowchart.menuState = 0;
-
-                        //$('.uf-side-bar.right').css('right', '-245px');
-                        $('.uf-side-bar.right')
-                            .resizable({
-                                minWidth: $('.uf-side-bar.right').width(),
-                                maxWidth: 1000,
-                                disabled: true,
-                                handles: "w" // https://api.jqueryui.com/1.8/resizable/#option-handles
-                            })
-                            .on('resize', function(evt) { // bug-fix: https://bugs.jqueryui.com/ticket/4985
-                                $(evt.target).css('left', '');
-                            });
+                    _.debounce(() => {
+                        //console.log('ready.jstree:', el); //, el.find('ul.jstree-container-ul').find('li.jstree-node'));
 
                         setTimeout(function() {
-                            $('.uf-side-bar.right').css('right', String(-($('.uf-side-bar.right').width() + 8) + 'px'));
+
+                            //console.log('ready.jstree1:', el, $(document).find('ul.jstree-container-ul li.jstree-node ul.jstree-children').children());
+                            el.jstree('open_all'); // expand/open all jstree's
+
+                            var LastId = '';
+                            $("a.jstree-anchor").each(function(ElIdx, ElVal) {
+                                //console.dir(ElVal);
+                                console.log('ready.jstree.elm:', { id: ElVal.id, idx: ElIdx });
+                                if (new RegExp('^workspace\-.*', 'g').test(LastId)) { // Click Open Project on 1st Element after 'Workspace'
+                                    _.debounce(function(oElVal) {
+                                        console.log('OpenProjDebounced:', { id: oElVal.id, idx: ElIdx });
+                                        oElVal.click();
+                                    }, 1000, { trailing: true })(ElVal);
+                                }
+                                LastId = ElVal.id;
+                                //OpenProjDebounced();
+                                //} // open project
+                            });
+                            $('.main-view').css('left', '0px');
                             window.$flowchart.menuState = 0;
-                            $('.uf-side-bar.left').css('left', '-245px');
-                            $('.uf-side-bar.left')
-                                .on('mouseover', function(evt) {
-                                    $('.uf-side-bar.left').css('left', '');
+
+                            //$('.uf-side-bar.right').css('right', '-245px');
+                            $('.uf-side-bar.right')
+                                .resizable({
+                                    minWidth: $('.uf-side-bar.right').width(),
+                                    maxWidth: 1000,
+                                    disabled: true,
+                                    handles: "w" // https://api.jqueryui.com/1.8/resizable/#option-handles
                                 })
-                                .on('mouseout', function(evt) {
-                                    //console.log('mouseout!!', evt.offsetX);
-                                    if (evt.toElement && evt.toElement.className.baseVal === 'flowchart-links-layer' && !window.$flowchart.menuState) {
-                                        setTimeout(function() {
-                                            $('.uf-side-bar.left').css('left', '-245px');
-                                        }, 350);
-                                    }
+                                .on('resize', function(evt) { // bug-fix: https://bugs.jqueryui.com/ticket/4985
+                                    $(evt.target).css('left', '');
                                 });
-                        }, 350);
-                    }, 500);
+
+                            setTimeout(function() {
+                                $('.uf-side-bar.right').css('right', String(-($('.uf-side-bar.right').width() + 8) + 'px'));
+                                window.$flowchart.menuState = 0;
+                                $('.uf-side-bar.left').css('left', '-245px');
+                                $('.uf-side-bar.left')
+                                    .on('mouseover', function(evt) {
+                                        $('.uf-side-bar.left').css('left', '');
+                                    })
+                                    .on('mouseout', function(evt) {
+                                        //console.log('mouseout!!', evt.offsetX);
+                                        if (evt.toElement && evt.toElement.className.baseVal === 'flowchart-links-layer' && !window.$flowchart.menuState) {
+                                            setTimeout(function() {
+                                                $('.uf-side-bar.left').css('left', '-245px');
+                                            }, 350);
+                                        }
+                                    });
+                            }, 350);
+                        }, 500);
+
+                    }, 700, { trailing: true })();
+
                 });
 
             // https://groups.google.com/forum/#!topic/jstree/BYppISuCFRE
