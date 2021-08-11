71a3819ff103a55da86132d0ad80965feb05a9c4
Cupidazul <cupidazul@gmail.com>
2021-08-11 14:52:15 +0100

    cosmetic code rearrangement

--- library/ultiflow/static/javascript/jquery.uf.process_main_infos.js
+++ library/ultiflow/static/javascript/jquery.uf.process_main_infos.js
@@ -15,7 +15,7 @@ define(['app'], function(app) {
 
             $app.ultiflow.uf_process_main_infos = self;
 
-            this.els.title = $('<span class="uf-process-title"></span>');
+            self.els.title = $('<span class="uf-process-title"></span>');
             //this.els.title.appendTo(this.element);
 
             var BtnStatus = '<button id="btn_ioStatus" class="btn">PyServer</button>';
@@ -23,20 +23,31 @@ define(['app'], function(app) {
             var BtnSave = '<button id="btn_save" title="Save" class="fa fa-save" style="width: 30px;font-size: 20px;padding-left: 4px;padding-top: 2px;"></button>';
             var BtnPlay = '<button id="btn_play" title="Run" class="fa fa-play" style="width: 30px;font-size: 20px;padding-left: 4px;padding-top: 2px;"></button>';
             var BtnSelect = ''; //'<select id="btn_code_lang" class="selectpicker" style="font-size: 14px;"><option>Perl</option><option>Python</option></select>';
-            this.els.buttons = $(`
-<nobr><div class="buttons" style="font-size: 10px;display: inline-flex;padding-left: 5%;height: -webkit-fill-available;position: relative;text-align: center;">${BtnSettings}&nbsp;${BtnSave}${BtnPlay}&nbsp;${BtnSelect}${BtnStatus}</div></nobr>
-<nobr><div class="buttons" style="float: right;font-size: 10px;display: flex;padding-top: 5px;"><button class="zoom-in">Zoom In</button><button class="zoom-out">Zoom Out</button><input type="range" class="zoom-range" step="0.5" min="0.5" max="3"><button class="zoom-reset">Reset</button></div></nobr>
-`);
-            this.els.buttons.appendTo(this.element);
-            this.els.title.prependTo(this.element.children('nobr')[0]); // 'Process Title': should go inside first nobr, topmost position.
+            self.els.buttons = $(`` +
+                `<nobr>` +
+                `   <div class="buttons" style="font-size: 10px;display: inline-flex;padding-left: 5%;height: -webkit-fill-available;position: relative;text-align: center;">` +
+                `       ${BtnSettings}&nbsp;${BtnSave}${BtnPlay}&nbsp;${BtnSelect}${BtnStatus}` +
+                `   </div>` +
+                `</nobr>` +
+                `<nobr>` +
+                `   <div class="buttons" style="float: right;font-size: 10px;display: flex;padding-top: 5px;">` +
+                `       <button class="zoom-in">Zoom In</button>` +
+                `       <button class="zoom-out">Zoom Out</button>` +
+                `       <input type="range" class="zoom-range" step="0.5" min="0.5" max="3">` +
+                `       <button class="zoom-reset">Reset</button>` +
+                `   </div>` +
+                `</nobr>` +
+                ``);
+            self.els.buttons.appendTo(self.element);
+            self.els.title.prependTo(self.element.children('nobr')[0]); // 'Process Title': should go inside first nobr, topmost position.
 
-            this.els.state = $('<span class="uf-process-state"><span class="uf-process-state-saving">Saving changes...</span><span class="uf-process-state-saved">Changes saved.</span><span class="uf-process-state-error">Error!</span></span>');
-            this.els.state.appendTo(this.element);
+            self.els.state = $('<span class="uf-process-state"><span class="uf-process-state-saving">Saving changes...</span><span class="uf-process-state-saved">Changes saved.</span><span class="uf-process-state-error">Error!</span></span>');
+            self.els.state.appendTo(self.element);
 
-            this.els.path = $('<span class="uf-process-path"></span>');
-            this.els.path.appendTo(this.element);
+            self.els.path = $('<span class="uf-process-path"></span>');
+            self.els.path.appendTo(self.element);
 
-            app.onEvent('ultiflow::process_open', function(e, operatorData) {
+            app.onEvent('ultiflow::process_open', function(evt, operatorData) {
                 self.setProcess(operatorData);
             });
 
@@ -44,7 +55,7 @@ define(['app'], function(app) {
                 self.setState('saving');
             });
 
-            app.onEvent('ultiflow::process_saved', function(e, success) {
+            app.onEvent('ultiflow::process_saved', function(evt, success) {
                 if (success) {
                     self.setState('saved', 3000);
                 } else {
